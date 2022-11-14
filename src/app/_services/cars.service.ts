e24936987da6998a210fe99d034d2faddef67ca8
ismael <ismael.qunta.dev@gmail.com>
2022-11-14 23:18:13 +0100

    commit de front silvia arreglado

--- src/app/_services/cars.service.ts
+++ src/app/_services/cars.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 // const API_URL = 'http://localhost:8080/api/';
-const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
+ const API_URL = 'https://web-production-cc37.up.railway.app/api/';
+// const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
 
 @Injectable({
   providedIn: 'root'
--- src/app/_services/hello.service.ts
+++ src/app/_services/hello.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
-//const API_URL = 'http://localhost:8080/api/';
+// const API_URL = 'https://alansastre-springboot-psql.herokuapp.com/api/';
+// const API_URL = 'http://localhost:8080/api/';
+const API_URL = 'https://web-production-cc37.up.railway.app/api/';
 
 @Injectable({
   providedIn: 'root'
